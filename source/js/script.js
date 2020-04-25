fb835beca544a55ac53184b23b05745d776ef772
Мария <slinkywizard@mail.ru>
2020-04-25 12:08:21 +0300

    Устранение замечаний

--- source/js/script.js
+++ source/js/script.js
@@ -1,27 +1,4 @@
-/*****************************МОБИЛЬНОЕ МЕНЮ**********************************/
-/*
-var buttonBurger = document.querySelector(".burger-menu");
 
-var mobileMenu = document.querySelector(".site-nav");
-
-var closeNav = document.querySelector(".site-nav__close");
-
-mobileMenu.classList.add("site-nav--hide");
-
-buttonBurger.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  mobileMenu.classList.toggle("site-nav--hide");
-  mobileMenu.classList.add("site-nav--show");
-  buttonBurger.classList.add("burger-menu--hide");
-});
-
-closeNav.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  mobileMenu.classList.remove("site-nav--show");
-  mobileMenu.classList.add("site-nav--hide");
-  buttonBurger.classList.remove("burger-menu--hide");
-});
-*/
 /*****************************ОТПРАВКА ФОРМЫ, МОДАЛЬНЫЕ ОКНА******************************/
 var button = document.querySelector(".form-review__button");
 
@@ -51,22 +28,22 @@ try {
   isStorageSupport = false;
 }
 
-//popupsuccess.classList.add("popup-success--hide");
-//popuperror.classList.add("popup-error--hide");
+popupSuccess.classList.add("popup-success--hide");
+popupError.classList.add("popup-error--hide");
 
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
   console.log("yrtrurrww");
-  //alert("начало");
+  alert("начало");
   if (!userName.value || !userEmail.value || !userMessage.value) {
 
     evt.preventDefault();
     popupError.classList.remove("popup-error--show");
     popupError.offsetWidth = popupError.offsetWidth;
     popupError.classList.add("popup-error--show");
-    //alert("пусто");
+    alert("пусто");
   } else if (isStorageSupport) {
-    //alert("заполнили таки");
+    alert("заполнили таки");
     localStorage.setItem("user-name", userName.value);
     localStorage.setItem("user-email", userEmail.value);
     popupSuccess.classList.add("popup-success--show");
@@ -111,10 +88,9 @@ window.addEventListener("keydown", function (evt) {
   }
 });
 
-/*
 form.addEventListener("submit", function (evt) {
   if (isStorageSupport) {
     localStorage.setItem("userName", userName.value);
   }
 });
-*/
+
