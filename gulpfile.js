225997fa989d23cb8b067add149d63ed0fc07c03
Мария <slinkywizard@mail.ru>
2020-04-28 23:13:12 +0300

    Изменила цвет видео-кнопок, текст Седона на странице Фото, Форма в блоке Интро

--- gulpfile.js
+++ gulpfile.js
@@ -55,7 +55,7 @@ gulp.task("webp", function () {
 });
 
 gulp.task("sprite", function () {
-  return gulp.src("source/img/icon-*.svg")
+  return gulp.src(["source/img/icon-*.svg", "source/img/htmlacademy.svg"])
     .pipe(svgstore({
       inlineSvg: true
     }))
