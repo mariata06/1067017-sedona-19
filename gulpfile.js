e1d0be8eaebb66c3dc3568422359d0153a4875e9
Мария <slinkywizard@mail.ru>
2020-04-30 18:37:36 +0300

    Начало выполнения 9 Задания Производительность верстки

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ var svgstore = require("gulp-svgstore");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
 var del = require("del");
+var htmlmin = require("gulp-htmlmin");
 
 
 gulp.task("css", function () {
@@ -68,6 +69,9 @@ gulp.task("html", function () {
     .pipe(posthtml([
       include()
     ]))
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
     .pipe(gulp.dest("build"));
 });
 
