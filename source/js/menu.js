1e413ecc8b36952d2a19719e7687e03d377d202f
Мария <slinkywizard@mail.ru>
2020-05-07 22:20:34 +0300

    Ровнение с пиксель перфект, доработка menu.js

--- source/js/menu.js
+++ source/js/menu.js
@@ -6,14 +6,7 @@ var mobileMenu = document.querySelector(".site-nav");
 
 var closeNav = document.querySelector(".site-nav__close");
 
-//mobileMenu.classList.add("site-nav--hide"); // спрятанное моб меню при работающем js
-//closeNav.classList.add("site-nav__close--hide");
-//document.addEventListener("DOMContentLoaded", function (evt) {
-// alert("Тест");
-//buttonBurger.classList.add("burger-menu--show");
-//});
-
-alert("Тест");
+mobileMenu.classList.add("site-nav--hide"); // спрятанное моб меню при работающем js
 buttonBurger.classList.remove("burger-menu--nojs");
 closeNav.classList.remove("site-nav__close--nojs");
 
@@ -22,7 +15,6 @@ buttonBurger.addEventListener("click", function (evt) {
   mobileMenu.classList.toggle("site-nav--hide");
   mobileMenu.classList.add("site-nav--show");
   buttonBurger.classList.add("burger-menu--hide");
-  //closeNav.classList.add("site-nav__close--show");
 });
 
 closeNav.addEventListener("click", function (evt) {
