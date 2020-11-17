2301c08fb6bab21b0f4c9d27b572bdf4203e697e
Мария <slinkywizard@mail.ru>
2020-11-17 15:35:17 +0300

    Updates

---
+++ js/menu.js
@@ -0,0 +1,25 @@
+/*****************************МОБИЛЬНОЕ МЕНЮ**********************************/
+
+var buttonBurger = document.querySelector(".burger-menu");
+
+var mobileMenu = document.querySelector(".site-nav");
+
+var closeNav = document.querySelector(".site-nav__close");
+
+mobileMenu.classList.add("site-nav--hide"); // спрятанное моб меню при работающем js
+buttonBurger.classList.remove("burger-menu--nojs");
+closeNav.classList.remove("site-nav__close--nojs");
+
+buttonBurger.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mobileMenu.classList.toggle("site-nav--hide");
+  mobileMenu.classList.add("site-nav--show");
+  buttonBurger.classList.add("burger-menu--hide");
+});
+
+closeNav.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mobileMenu.classList.remove("site-nav--show");
+  mobileMenu.classList.add("site-nav--hide");
+  buttonBurger.classList.remove("burger-menu--hide");
+});
